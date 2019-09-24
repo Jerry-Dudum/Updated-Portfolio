b96711be546ead7da147929c78763d302cfb571a
Jerry Dudum <jsdudum@gmail.com>
2019-09-23 19:47:47 -0700

    changed hover functionality to be strictly css because I noticed using javascript to do it was adding unwanted animations after you unhovered it

--- assets/javascript/app.js
+++
@@ -1,7 +0,0 @@
-$(".icons").hover(function () {
-    $(this).addClass("animated tada");
-});
-
-$(".icons").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function () {
-    $(this).removeClass("animated tada");
-});
